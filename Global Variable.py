18de2644f131f69f64e0c5566f09c1a86dbadee4
Artificial2003 <limbusurendra713@gmail.com>
2025-05-03 18:16:14 +0545

    Added Global vaiable script

---
+++ Global Variable.py
@@ -0,0 +1,9 @@
+#global variable
+name='TutorialsPoint'
+marks=50
+def myfunction():
+    #accessing inside the function
+    print("name:",name)
+    print("marks:",marks)
+#function call
+myfunction()
