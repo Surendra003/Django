cce285fc395c05e5e1f962145961089b979cdca6
Artificial2003 <limbusurendra713@gmail.com>
2025-05-05 13:41:56 +0545

    Adding feature of if condition

--- python.py
+++ python.py
@@ -4,7 +4,11 @@ b=int(input("Enter a Second Number:"))
 c=a+b
 print(type(a))
 print(f"The sum is {c}")
-if a!=b:
-    print(f"{a} is not eqal to {b}")
-else:
-    print("a is some value")
+if a != b:
+    print(f"{a} is not equal to {b}")
+if a == b:
+    print(f"{a} is equal to {b}")
+if a < b:
+    print(f"{a} is greater than {b} ")
+if a > b:
+    print(f"{a} is greater than {b}")
