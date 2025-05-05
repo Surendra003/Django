7cdbd464f6658b61fe4e25882897b8208a5c16b9
Artificial2003 <limbusurendra713@gmail.com>
2025-05-05 21:11:42 +0545

    Adding assignment as 1

---
+++ Day2/1.Assignment Operator.py
@@ -0,0 +1,28 @@
+global x
+x=10
+print(x)
+x+=5    #x=x+5
+
+print(x)
+
+x-=3    #x=x-5
+print(x)
+
+x*=2
+print(x)
+
+x=10
+x/=2    #x=x/2
+print(x)
+
+x=10
+x//=3
+print(x)
+
+x=10
+x%=3    #x=x%3 "Modulus"
+print(x)
+
+x=10
+x**=2
+print(x)
