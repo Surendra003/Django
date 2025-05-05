41b35e7302247bb755a0ed0c509728ceb5a12aa1
Artificial2003 <limbusurendra713@gmail.com>
2025-05-05 13:23:59 +0545

    printing globals

--- print Locals.py
+++ print Locals.py
@@ -1,9 +1,11 @@
 name='TutorialPoints'
 marks=50
 result=True
+print("Hello World")
 def myfunction():
     a=10
     b=20
     c=a+b
     print("globals():",globals)#local ma actually variable huneraxa
     return c
+myfunction()
