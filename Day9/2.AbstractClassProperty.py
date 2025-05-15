ebf640a9822bae245fc7eb1a67aaf897c71ff1db
Artificial2003 <limbusurendra713@gmail.com>
2025-05-15 15:10:07 +0545

    Day 9

---
+++ Day9/2.AbstractClassProperty.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    @property
+    @abstractmethod
+    def sound(self):
+        pass
+
+class Bird(Animal):
+    @property
+    def sound(self):
+        return "Chirp"
+
+bird = Bird()
+print(bird.sound)
